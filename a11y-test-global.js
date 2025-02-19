5561820751c447a326ef457a8576e5983890984b
MaximilianGcodes <maximilian.giesen@denkwerk.com>
2025-02-19 15:10:16 +0100

    refactor project

--- a11y-test-global.js
+++ a11y-test-global.js
@@ -8,7 +8,7 @@ const axios = require('axios'); // Lädt Webseiten-Inhalte (HTML) mit HTTP-Anfra
 const cheerio = require('cheerio');// Parst das HTML, um Links zu extrahieren.
 const path = require('path'); // Arbeitet mit Dateipfaden.
 
-const baseUrl = 'https://migration-arag-de-cms.arag.com/krankenzusatzversicherung/krankentagegeldversicherung/index.html';
+const baseUrl = 'https://parkweiher.koeln';
 const visitedUrls = new Set();
 const resultsDir = 'accessibility-results';
 
